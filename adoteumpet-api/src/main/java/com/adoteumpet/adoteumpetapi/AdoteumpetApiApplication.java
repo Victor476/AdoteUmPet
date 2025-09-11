94499833d182d00bd03ff9363af4f0f1eee5e5bf
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2025-09-11 16:06:01 -0300

    subindo o basico

---
+++ adoteumpet-api/src/main/java/com/adoteumpet/adoteumpetapi/AdoteumpetApiApplication.java
@@ -0,0 +1,13 @@
+package com.adoteumpet.adoteumpetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteumpetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteumpetApiApplication.class, args);
+	}
+
+}
---
+++ adoteumpet-api/src/test/java/com/adoteumpet/adoteumpetapi/AdoteumpetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoteumpet.adoteumpetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteumpetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
