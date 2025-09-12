16a5ae175170fce986432cac0d16935922834194
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2025-09-12 18:29:30 -0300

    subindo alterações nos testes

--- adoteumpet-api/src/test/java/com/adoteumpet/adoteumpetapi/AdoteumpetApiApplicationTests.java
+++ adoteumpet-api/src/test/java/com/adoteumpet/adoteumpetapi/AdoteumpetApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.adoteumpet.adoteumpetapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AdoteumpetApiApplicationTests {
 
 	@Test
